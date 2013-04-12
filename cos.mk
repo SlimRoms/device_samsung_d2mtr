fb3b3b11fade84d5f480bdc153539d200b91fa8a
David Rouleau <Shabbypenguin@gmail.com>
2013-04-12 09:02:41 -0400

    Convert device tree

--- cos.mk
+++ cos.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/samsung/d2mtr/full_d2mtr.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
+# Inherit some common Cos stuff.
+$(call inherit-product, vendor/cos/config/cdma.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cos/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Cos stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2mtr TARGET_DEVICE=d2mtr BUILD_FINGERPRINT="samsung/d2mtr/d2mtr:4.0.4/IMM76D/R530MVQALJ1:user/release-keys" PRIVATE_BUILD_DESC="d2mtr-user 4.0.4 IMM76D R530MVQALJ1 release-keys"
 
-PRODUCT_NAME := cm_d2mtr
+PRODUCT_NAME := cos_d2mtr
 PRODUCT_DEVICE := d2mtr
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_d2mtr-eng
+add_lunch_combo cos_d2mtr-eng
