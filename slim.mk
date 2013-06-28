13a70dab8aca2f606ec9d4af34bf2e560a44991d
David Rouleau <Shabbypenguin@gmail.com>
2013-06-28 14:21:51 -0400

    Slim the tree
    Adjust the device tree to work on Slim roms

---
+++ slim.mk
@@ -0,0 +1,32 @@
+$(call inherit-product, device/samsung/d2mtr/full_d2mtr.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := d2mtr
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/cdma.mk)
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device settings
+$(call inherit-product, vendor/slim/config/common_sgs.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+#copy 00check
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
+
+#bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2mtr TARGET_DEVICE=d2mtr BUILD_FINGERPRINT="samsung/d2mtr/d2mtr:4.1.2/JZO54K/R530MVQAMB2:user/release-keys" PRIVATE_BUILD_DESC="d2mtr-user 4.1.2 JZO54K R530MVQAMB2 release-keys"
+
+PRODUCT_NAME := slim_d2mtr
+PRODUCT_DEVICE := d2mtr
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SCH-R530M
+PRODUCT_MANUFACTURER := Samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cos_d2mtr-eng
+add_lunch_combo slim_d2mtr-userdebug
