78fb4d0a52ca9869c241efc789f33affb6d07ef6
nicklovell23 <nicklovell23@gmail.com>
2014-11-26 19:21:37 -0600

    Update slim.mk Conflicts: 	slim.mk

--- slim.mk
+++ slim.mk
@@ -1,15 +1,18 @@
 $(call inherit-product, device/samsung/d2mtr/full_d2mtr.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := d2mtr
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2mtr TARGET_DEVICE=d2mtr BUILD_FINGERPRINT="samsung/d2mtr/d2mtr:4.1.2/JZO54K/R530MVQAMB2:user/release-keys" PRIVATE_BUILD_DESC="d2mtr-user 4.1.2 JZO54K R530MVQAMB2 release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2mtr \
+    TARGET_DEVICE=d2mtr \
+    BUILD_FINGERPRINT="samsung/d2mtr/d2mtr:4.1.2/JZO54K/R530MVQAMB2:user/release-keys"\
+    PRIVATE_BUILD_DESC="d2mtr-user 4.1.2 JZO54K R530MVQAMB2 release-keys"
 
 PRODUCT_NAME := slim_d2mtr
 PRODUCT_DEVICE := d2mtr
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SCH-R530M
-PRODUCT_MANUFACTURER := Samsung
+
